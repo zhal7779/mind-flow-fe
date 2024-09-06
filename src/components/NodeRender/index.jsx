d286da2d547d035b995d3b2baea53e81cdb16019
minyoung <zhal7779@naver.com>
2024-09-06 17:01:46 +0900

    Feat : 트리 양방향으로 렌더링 구현중

--- src/components/NodeRender/index.jsx
+++ src/components/NodeRender/index.jsx
@@ -84,18 +84,32 @@ const NodeRender = forwardRef((props, ref) => {
     </React.Fragment>
   ));
 
+  console.log(node.rightChildNode);
   return (
     <NodeContainer ref={ref}>
-      <Node
-        id={node.node}
-        $level={node.level}
-        onClick={() => node.node === 0 && setRootActive(true)}
-        onMouseEnter={handleMouseEnter}
-        onMouseLeave={handleMouseLeave}
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
       >
-        {isHovered &&
-          (node.level === 0 ? (
+        {/* 왼쪽 자식 노드 렌더링 */}
+        {/* <div style={{ flex: 1, marginRight: "50px" }}>
+          {node.leftChildNode.length > 0 && leftChildNodeRender}
+        </div> */}
+
+        {/* Root Topic */}
+        <Node
+          id={node.node}
+          $level={node.level}
+          onClick={() => node.node === 0 && setRootActive(true)}
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+        >
+          {isHovered && node.level === 0 && (
             <ButtonWrapper>
+              {/* 왼쪽에 추가/삭제 버튼 */}
               <Button
                 onClick={() => handleAddChild("left")}
                 style={{ right: "-1rem" }}
@@ -112,14 +126,8 @@ const NodeRender = forwardRef((props, ref) => {
               >
                 <FontAwesomeIcon icon={faMinus} />
               </Button>
-              <Button
-                onClick={() => handleDeleteNode("right")}
-                style={{ right: "-1rem" }}
-                $size={2}
-                $color={"var(--color-red)"}
-              >
-                <FontAwesomeIcon icon={faMinus} />
-              </Button>
+
+              {/* 오른쪽에 추가/삭제 버튼 */}
               <Button
                 onClick={() => handleAddChild("right")}
                 style={{ right: "-4rem" }}
@@ -128,68 +136,157 @@ const NodeRender = forwardRef((props, ref) => {
               >
                 <FontAwesomeIcon icon={faPlus} />
               </Button>
-            </ButtonWrapper>
-          ) : node.level !== 0 && node.side === "right" ? (
-            <ButtonWrapper>
               <Button
-                onClick={() => handleDeleteNode(node.side)}
-                style={{ right: "-1rem" }}
+                onClick={() => handleDeleteNode("right")}
+                style={{ right: "-4rem" }}
                 $size={2}
                 $color={"var(--color-red)"}
               >
                 <FontAwesomeIcon icon={faMinus} />
               </Button>
-              <Button
-                onClick={() => handleAddChild(node.side)}
-                style={{ right: "-4rem" }}
-                $size={2.6}
-                $color={"var(--color-blue)"}
-              >
-                <FontAwesomeIcon icon={faPlus} />
-              </Button>
             </ButtonWrapper>
+          )}
+
+          {/* Root Topic Input */}
+          {node.level === 0 ? (
+            <RootTopicInput
+              rows={1}
+              onChange={(e) => updateNodeInputValue(e, node)}
+              value={node.value}
+              placeholder="메인 주제를 입력해주세요"
+            />
+          ) : node.level === 1 ? (
+            <MainTopicInput
+              rows={1}
+              onChange={(e) => updateNodeInputValue(e, node)}
+              value={node.value}
+              placeholder="브랜치 주제를 입력해주세요"
+            />
           ) : (
-            <></>
-          ))}
+            <ContentInput
+              rows={1}
+              onChange={(e) => updateNodeInputValue(e, node)}
+              value={node.value}
+              placeholder="내용을 입력해주세요"
+            />
+          )}
+          {node.node > 0 && <NodeLine {...lineProps} />}
+        </Node>
 
-        {node.level === 0 ? (
-          <RootTopicInput
-            rows={1}
-            onChange={(e) => updateNodeInputValue(e, node)}
-            value={node.value}
-            placeholder="메인 주제를 입력해주세요"
-          ></RootTopicInput>
-        ) : node.level === 1 ? (
-          <MainTopicInput
-            rows={1}
-            onChange={(e) => updateNodeInputValue(e, node)}
-            value={node.value}
-            placeholder="브랜치 주제를 입력해주세요"
-          ></MainTopicInput>
-        ) : (
-          <ContentInput
-            rows={1}
-            onChange={(e) => updateNodeInputValue(e, node)}
-            value={node.value}
-            placeholder="내용을 입력해주세요"
-          ></ContentInput>
-        )}
-        {node.node > 0 && <NodeLine {...lineProps} />}
-      </Node>
-
-      {node.level === 0 ? (
-        node.leftChildNode.length > 0 ? (
-          <div style={{ marginRight: "100px" }}>{leftChildNodeRender}</div>
-        ) : (
-          <div style={{ marginLeft: "100px" }}>{rightChildNodeRender}</div>
-        )
-      ) : (
-        node.childNode.length > 0 && (
-          <div style={{ marginLeft: "100px" }}>{childNodeRender}</div>
-        )
-      )}
+        {/* 오른쪽 자식 노드 렌더링 */}
+        <div style={{ flex: 1, marginLeft: "50px" }}>
+          {node.rightChildNode.length > 0 && rightChildNodeRender}
+        </div>
+      </div>
     </NodeContainer>
   );
+
+  // return (
+  //   <NodeContainer ref={ref}>
+  //     <Node
+  //       id={node.node}
+  //       $level={node.level}
+  //       onClick={() => node.node === 0 && setRootActive(true)}
+  //       onMouseEnter={handleMouseEnter}
+  //       onMouseLeave={handleMouseLeave}
+  //     >
+  //       {isHovered &&
+  //         (node.level === 0 ? (
+  //           <ButtonWrapper>
+  //             <Button
+  //               onClick={() => handleAddChild("left")}
+  //               style={{ right: "-1rem" }}
+  //               $size={2.6}
+  //               $color={"var(--color-blue)"}
+  //             >
+  //               <FontAwesomeIcon icon={faPlus} />
+  //             </Button>
+  //             <Button
+  //               onClick={() => handleDeleteNode("left")}
+  //               style={{ right: "-1rem" }}
+  //               $size={2}
+  //               $color={"var(--color-red)"}
+  //             >
+  //               <FontAwesomeIcon icon={faMinus} />
+  //             </Button>
+  //             <Button
+  //               onClick={() => handleDeleteNode("right")}
+  //               style={{ right: "-1rem" }}
+  //               $size={2}
+  //               $color={"var(--color-red)"}
+  //             >
+  //               <FontAwesomeIcon icon={faMinus} />
+  //             </Button>
+  //             <Button
+  //               onClick={() => handleAddChild("right")}
+  //               style={{ right: "-4rem" }}
+  //               $size={2.6}
+  //               $color={"var(--color-blue)"}
+  //             >
+  //               <FontAwesomeIcon icon={faPlus} />
+  //             </Button>
+  //           </ButtonWrapper>
+  //         ) : node.level !== 0 && node.side === "right" ? (
+  //           <ButtonWrapper>
+  //             <Button
+  //               onClick={() => handleDeleteNode(node.side)}
+  //               style={{ right: "-1rem" }}
+  //               $size={2}
+  //               $color={"var(--color-red)"}
+  //             >
+  //               <FontAwesomeIcon icon={faMinus} />
+  //             </Button>
+  //             <Button
+  //               onClick={() => handleAddChild(node.side)}
+  //               style={{ right: "-4rem" }}
+  //               $size={2.6}
+  //               $color={"var(--color-blue)"}
+  //             >
+  //               <FontAwesomeIcon icon={faPlus} />
+  //             </Button>
+  //           </ButtonWrapper>
+  //         ) : (
+  //           <></>
+  //         ))}
+
+  //       {node.level === 0 ? (
+  //         <RootTopicInput
+  //           rows={1}
+  //           onChange={(e) => updateNodeInputValue(e, node)}
+  //           value={node.value}
+  //           placeholder="메인 주제를 입력해주세요"
+  //         ></RootTopicInput>
+  //       ) : node.level === 1 ? (
+  //         <MainTopicInput
+  //           rows={1}
+  //           onChange={(e) => updateNodeInputValue(e, node)}
+  //           value={node.value}
+  //           placeholder="브랜치 주제를 입력해주세요"
+  //         ></MainTopicInput>
+  //       ) : (
+  //         <ContentInput
+  //           rows={1}
+  //           onChange={(e) => updateNodeInputValue(e, node)}
+  //           value={node.value}
+  //           placeholder="내용을 입력해주세요"
+  //         ></ContentInput>
+  //       )}
+  //       {node.node > 0 && <NodeLine {...lineProps} />}
+  //     </Node>
+
+  //     {node.level === 0 ? (
+  //       node.leftChildNode.length > 0 ? (
+  //         <div style={{ marginRight: "100px" }}>{leftChildNodeRender}</div>
+  //       ) : (
+  //         <div style={{ marginLeft: "100px" }}>{rightChildNodeRender}</div>
+  //       )
+  //     ) : (
+  //       node.childNode.length > 0 && (
+  //         <div style={{ marginLeft: "100px" }}>{childNodeRender}</div>
+  //       )
+  //     )}
+  //   </NodeContainer>
+  // );
 });
 
 export default NodeRender;
