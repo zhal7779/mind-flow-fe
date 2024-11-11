dbb62d25d8b4f2f937df2742f114029d6ff606a3
minyoung <zhal7779@naver.com>
2024-11-11 19:58:28 +0900

    Feat: tree 파일 재정의

---
+++ src/types/fileType.ts
@@ -0,0 +1,39 @@
+interface Position {
+  x: number;
+  y: number;
+  r: number;
+  t: number;
+}
+
+interface ParentNode {
+  node: number;
+  position: Position;
+}
+
+interface BaseNode {
+  value: string;
+  node: number;
+  level: number;
+  position: Position;
+}
+
+interface ChildNode extends BaseNode {
+  side: string;
+  parentNode: ParentNode;
+}
+
+export interface GeneralNode extends ChildNode {
+  childNode: Node[] | [];
+}
+
+export interface RootNode extends BaseNode {
+  parentNode: ParentNode;
+  leftChildNode: GeneralNode[] | [];
+  rightChildNode: GeneralNode[] | [];
+}
+
+export interface FileList {
+  fileName: string;
+  updatedDate: string;
+  tree: RootNode;
+}
