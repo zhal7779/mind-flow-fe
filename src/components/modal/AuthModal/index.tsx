8a2a853fd57372d8d5d9e963f28e79f5c4912dc3
Minyoung Lee <zhal7779@gmail.com>
2024-11-22 22:56:46 +0900

    Design: 모달 스타일 공통 스타일로  분리

--- src/components/modal/AuthModal/index.tsx
+++ src/components/modal/AuthModal/index.tsx
@@ -1,14 +1,14 @@
-import { useRecoilState, useSetRecoilState } from "recoil";
-import { authState, isOpenAuthModal } from "../../../recoil/atoms/auth";
-import BaseModal from "../BaseModal";
-import styled from "styled-components";
-import { useState } from "react";
-import { alert } from "../../../utils/alert";
-
+import { useRecoilState, useSetRecoilState } from 'recoil';
+import { authState, isOpenAuthModal } from '../../../recoil/atoms/auth';
+import BaseModal from '../BaseModal';
+import styled from 'styled-components';
+import { useState } from 'react';
+import { alert } from '../../../utils/alert';
+import * as S from '../../../styles/modal';
 const AuthModal = () => {
   const [isOpen, setIsOpen] = useRecoilState(isOpenAuthModal);
   const setAuth = useSetRecoilState(authState);
-  const [step, setStep] = useState("login"); // login => 로그인 화면, join-id => 회원가입 아이디 화면, join-pw =>회원가입 비밀번호 화면
+  const [step, setStep] = useState('login'); // login => 로그인 화면, join-id => 회원가입 아이디 화면, join-pw =>회원가입 비밀번호 화면
   const handleActiveTrial = () => {
     setAuth(true);
     setIsOpen(false);
@@ -19,73 +19,73 @@ const AuthModal = () => {
   };
 
   const handleCompleteJoin = () => {
-    alert("회원가입이 완료되었습니다", "success");
-    setStep("login");
+    alert('회원가입이 완료되었습니다', 'success');
+    setStep('login');
   };
 
   const LoginContent = (
-    <Content>
-      <Title>로그인</Title>
-      <InputContent>
+    <S.ModalContent>
+      <S.ModalTitle>로그인</S.ModalTitle>
+      <S.InputContent>
         <span>아이디</span>
-        <Input placeholder="아이디를 입력해주세요" />
-      </InputContent>
-      <InputContent>
+        <S.Input placeholder="아이디를 입력해주세요" />
+      </S.InputContent>
+      <S.InputContent>
         <span>비밀번호</span>
-        <Input placeholder="비밀번호를 입력해주세요" type="password" />
+        <S.Input placeholder="비밀번호를 입력해주세요" type="password" />
         <TextButton>비밀번호 찾기</TextButton>
-      </InputContent>
-      <ButtonWrapper>
-        <LoginButton>로그인</LoginButton>
-        <TrialLoginButton onClick={handleActiveTrial}>
+      </S.InputContent>
+      <S.ButtonWrapper>
+        <S.LoginButton>로그인</S.LoginButton>
+        <S.TrialLoginButton onClick={handleActiveTrial}>
           체험판 로그인
-        </TrialLoginButton>
-      </ButtonWrapper>
+        </S.TrialLoginButton>
+      </S.ButtonWrapper>
       <BottomContent>
         <span>계정이 없으신가요?</span>
         <TextButton
           onClick={() => {
-            setStep("join-id");
+            setStep('join-id');
           }}
         >
           회원가입
         </TextButton>
       </BottomContent>
-    </Content>
+    </S.ModalContent>
   );
 
   const JoinContent = (
-    <Content>
-      <Title>회원가입</Title>
-      {step === "join-id" ? (
+    <S.ModalContent>
+      <S.ModalTitle>회원가입</S.ModalTitle>
+      {step === 'join-id' ? (
         <>
-          <InputContent>
+          <S.InputContent>
             <span>닉네임</span>
-            <Input placeholder="닉네임을 입력해주세요" />
-          </InputContent>
-          <InputContent>
+            <S.Input placeholder="닉네임을 입력해주세요" />
+          </S.InputContent>
+          <S.InputContent>
             <span>아이디</span>
-            <Input placeholder="아이디를 입력해주세요" />
-          </InputContent>
+            <S.Input placeholder="아이디를 입력해주세요" />
+          </S.InputContent>
 
-          <LoginButton onClick={() => setStep("join-pw")}>계속</LoginButton>
+          <S.LoginButton onClick={() => setStep('join-pw')}>계속</S.LoginButton>
         </>
-      ) : step === "join-pw" ? (
+      ) : step === 'join-pw' ? (
         <>
-          <InputContent>
+          <S.InputContent>
             <span>비밀번호</span>
-            <Input placeholder="비밀번호를 입력해주세요" type="password" />
-          </InputContent>
-          <InputContent>
+            <S.Input placeholder="비밀번호를 입력해주세요" type="password" />
+          </S.InputContent>
+          <S.InputContent>
             <span>비밀번호 재확인</span>
-            <Input placeholder="비밀번호를 재확인해주세요" type="password" />
-          </InputContent>
-          <ButtonWrapper>
-            <TrialLoginButton onClick={() => setStep("join-id")}>
+            <S.Input placeholder="비밀번호를 재확인해주세요" type="password" />
+          </S.InputContent>
+          <S.ButtonWrapper>
+            <S.TrialLoginButton onClick={() => setStep('join-id')}>
               이전으로
-            </TrialLoginButton>
-            <LoginButton onClick={handleCompleteJoin}>완료</LoginButton>
-          </ButtonWrapper>
+            </S.TrialLoginButton>
+            <S.LoginButton onClick={handleCompleteJoin}>완료</S.LoginButton>
+          </S.ButtonWrapper>
         </>
       ) : (
         <></>
@@ -94,92 +94,24 @@ const AuthModal = () => {
         <span>이미 계정이 있으신가요?</span>
         <TextButton
           onClick={() => {
-            setStep("login");
+            setStep('login');
           }}
         >
           로그인하러 가기
         </TextButton>
       </BottomContent>
-    </Content>
+    </S.ModalContent>
   );
 
   return (
     <BaseModal isOpen={isOpen} onClose={handleDelete}>
-      <img src="/public/img/logo.png" alt="logo" style={{ width: "12rem" }} />
-      {step === "login" ? LoginContent : JoinContent}
+      <img src="/public/img/logo.png" alt="logo" style={{ width: '12rem' }} />
+      {step === 'login' ? LoginContent : JoinContent}
     </BaseModal>
   );
 };
 
 export default AuthModal;
-
-const Content = styled.div`
-  text-align: center;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  padding: 0 6rem;
-`;
-
-const Title = styled.h5`
-  margin: 2rem 0 4rem 0;
-  font-size: 2.2rem;
-  font-weight: 600;
-`;
-
-const InputContent = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 0.6rem;
-  align-items: flex-start;
-  width: 100%;
-  margin-bottom: 1.4rem;
-
-  > span {
-    font-weight: 600;
-    font-size: 1.3rem;
-  }
-`;
-
-const Input = styled.input`
-  width: 100%;
-  height: 4.8rem;
-  border: 1px solid var(--color-border);
-  border-radius: 0.8rem;
-  padding: 0 1rem;
-  font-size: 1.3rem;
-
-  &:hover,
-  &:focus {
-    background-color: var(--color-grey-06);
-  }
-`;
-
-const ButtonWrapper = styled.div`
-  display: flex;
-  gap: 1rem;
-  flex-direction: column;
-  width: 100%;
-`;
-
-const Button = styled.button`
-  width: 100%;
-  border-radius: 0.8rem;
-  font-size: 1.6rem;
-  padding: 1rem 0;
-  height: 4.8rem;
-`;
-
-const LoginButton = styled(Button)`
-  color: var(--color-white-bg);
-  background-color: var(--color-tag-purple);
-`;
-
-const TrialLoginButton = styled(Button)`
-  color: var(--color-tag-purple);
-  background-color: var(--color-white-bg);
-  border: 1px solid var(--color-tag-purple);
-`;
 
 const TextButton = styled.button`
   font-size: 1.3rem;
---
+++ src/styles/modal.tsx
@@ -0,0 +1,68 @@
+import styled from 'styled-components';
+export const ModalContent = styled.div`
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 0 6rem;
+`;
+
+export const ModalTitle = styled.h5`
+  margin: 2rem 0 4rem 0;
+  font-size: 2.2rem;
+  font-weight: 600;
+`;
+
+export const InputContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.6rem;
+  align-items: flex-start;
+  width: 100%;
+  margin-bottom: 1.4rem;
+
+  > span {
+    font-weight: 600;
+    font-size: 1.3rem;
+  }
+`;
+
+export const Input = styled.input`
+  width: 100%;
+  height: 4.8rem;
+  border: 1px solid var(--color-border);
+  border-radius: 0.8rem;
+  padding: 0 1rem;
+  font-size: 1.3rem;
+
+  &:hover,
+  &:focus {
+    background-color: var(--color-grey-06);
+  }
+`;
+
+export const ButtonWrapper = styled.div`
+  display: flex;
+  gap: 1rem;
+  flex-direction: column;
+  width: 100%;
+`;
+
+export const Button = styled.button`
+  width: 100%;
+  border-radius: 0.8rem;
+  font-size: 1.6rem;
+  padding: 1rem 0;
+  height: 4.8rem;
+`;
+
+export const LoginButton = styled(Button)`
+  color: var(--color-white-bg);
+  background-color: var(--color-tag-purple);
+`;
+
+export const TrialLoginButton = styled(Button)`
+  color: var(--color-tag-purple);
+  background-color: var(--color-white-bg);
+  border: 1px solid var(--color-tag-purple);
+`;
