957f1078d49ff8d7e47707fc66548c51553d62f7
Minyoung Lee <zhal7779@gmail.com>
2024-11-24 01:46:42 +0900

    Feat: 에디터 페이지 테마 변경 메뉴 구현

--- src/components/menu/SaveControlMenu/index.tsx
+++ src/components/menu/SaveControlMenu/index.tsx
@@ -2,15 +2,32 @@ import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import * as S from '../../../styles/menu';
 import styled from 'styled-components';
-import { faChevronLeft } from '@fortawesome/free-solid-svg-icons';
+import {
+  faChevronLeft,
+  faCircle,
+  faPalette,
+} from '@fortawesome/free-solid-svg-icons';
 import { HiOutlineSave } from 'react-icons/hi';
 import { fileDataState } from '../../../recoil/atoms/fileDataState';
-import { useRecoilValue } from 'recoil';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
 import { useNavigate } from 'react-router-dom';
+import { nodeColor } from '../../../recoil/atoms/nodeColor';
+
+const ThemeColors = [
+  { red: '#F26957' },
+  { pink: '#F25C9B' },
+  { orange: '#F69918' },
+  { green: '#30BEB9' },
+  { blue: '#48A0F7' },
+  { navy: '#6066FC' },
+  { purple: '#C263E8' },
+];
+
 const SaveControlMenu = () => {
+  const setColor = useSetRecoilState(nodeColor);
   const fileData = useRecoilValue(fileDataState);
   const [fileName, setFileName] = useState(fileData[0].fileName);
-
+  const [paletteActive, setPaletteActive] = useState(false);
   const navigate = useNavigate();
 
   const onChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -32,6 +49,27 @@ const SaveControlMenu = () => {
       <S.MenuIcon>
         <HiOutlineSave fontSize={'2rem'} />
       </S.MenuIcon>
+      <span></span>
+      <S.MenuIcon onClick={() => setPaletteActive(!paletteActive)}>
+        <FontAwesomeIcon icon={faPalette} fontSize={'2rem'} />
+      </S.MenuIcon>
+      {paletteActive && (
+        <PaletteMenu>
+          <ul>
+            {ThemeColors.map((item) => {
+              return Object.entries(item).map(([colorName, colorValue]) => (
+                <li key={colorName} onClick={() => setColor(colorName)}>
+                  <FontAwesomeIcon
+                    icon={faCircle}
+                    fontSize={'2rem'}
+                    color={colorValue}
+                  />
+                </li>
+              ));
+            })}
+          </ul>
+        </PaletteMenu>
+      )}
     </Wrapper>
   );
 };
@@ -47,4 +85,12 @@ const Wrapper = styled(S.MenuContainer)`
 const Input = styled.input`
   width: 24rem;
   margin-left: 1rem;
+`;
+
+const PaletteMenu = styled.div`
+  position: absolute;
+  > ul {
+    display: flex;
+    gap: 0.4rem;
+  }
 `;
