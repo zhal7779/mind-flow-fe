81c7a7856cc994fae1959d948e7321206266f9ef
Minyoung Lee <zhal7779@gmail.com>
2024-12-01 12:44:09 +0900

    Remove: 빌드위해 불필요한 호출 삭제

--- src/components/data/FileDataRender/index.tsx
+++ src/components/data/FileDataRender/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import * as S from './styles';
 import { CheckBox, DeleteButton } from '../../../styles/common';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
