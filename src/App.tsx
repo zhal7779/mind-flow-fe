c34c5d62fa26dfa228e5001a37e619e7d163adab
minyoung <zhal7779@naver.com>
2024-12-02 13:59:48 +0900

    Fix: 액세스 토큰 재발급 무한 호출 에러 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,34 @@
-import { GlobalStyle } from './GlobalStyle';
-import Editor from './pages/Editor';
-import { useRecoilValue, useSetRecoilState } from 'recoil';
-import { isDarkModeState } from './recoil/atoms/isDarkModeState';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Home from './pages/Home';
-import Trash from './pages/Trash';
-import MainLayout from './layouts/MainLayout';
-import AuthModal from './components/modal/AuthModal';
-import MypageModal from './components/modal/MypageModal';
-import { ThemeProvider } from 'styled-components';
-import theme from './data/theme';
-import Bookmark from './pages/Bookmark';
-import NotFoundPage from './pages/NotFound';
-import { useEffect } from 'react';
+import { GlobalStyle } from "./GlobalStyle";
+import Editor from "./pages/Editor";
+import { useRecoilValue, useSetRecoilState } from "recoil";
+import { isDarkModeState } from "./recoil/atoms/isDarkModeState";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
+import Trash from "./pages/Trash";
+import MainLayout from "./layouts/MainLayout";
+import AuthModal from "./components/modal/AuthModal";
+import MypageModal from "./components/modal/MypageModal";
+import { ThemeProvider } from "styled-components";
+import theme from "./data/theme";
+import Bookmark from "./pages/Bookmark";
+import NotFoundPage from "./pages/NotFound";
+import { useEffect } from "react";
 import {
   fetchAccessToken,
   clearAccessToken,
   getAccessToken,
-} from './utils/auth';
-import { authState } from './recoil/atoms/auth';
+} from "./utils/auth";
+import { authState } from "./recoil/atoms/auth";
 
 const App = () => {
   const setAuth = useSetRecoilState(authState);
   const isDarkMode = useRecoilValue(isDarkModeState);
   const token = getAccessToken();
-  console.log(token);
+
   useEffect(() => {
     const initializeAuth = async () => {
       const success = await fetchAccessToken();
+
       if (success) {
         setAuth(true);
       } else {
@@ -35,31 +36,36 @@ const App = () => {
         setAuth(false);
       }
     };
+    if (token) {
+      initializeAuth();
+    }
+  }, []);
 
-    initializeAuth();
-  }, [setAuth]);
+  useEffect(() => {
+    console.log("sdfsd");
+  }, []);
 
   const router = createBrowserRouter([
     {
-      path: '/',
+      path: "/",
       element: <MainLayout />,
       children: [
-        { path: '/', element: <Home /> },
+        { path: "/", element: <Home /> },
         {
-          path: '/editor/:id',
+          path: "/editor/:id",
           element: <Editor />,
         },
         {
-          path: '/bookmark/:tag',
+          path: "/bookmark/:tag",
           element: <Bookmark />,
         },
         {
-          path: '/trash',
+          path: "/trash",
           element: <Trash />,
         },
       ],
     },
-    { path: '*', element: <NotFoundPage /> },
+    { path: "*", element: <NotFoundPage /> },
   ]);
 
   return (
